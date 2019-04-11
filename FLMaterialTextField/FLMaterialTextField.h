798859d02c6d44d6054c421e2bdb523ceebe672e
K2 - Felipe Augusto Pimentel P <k2.felipe@MacBook-Pro-de-user-local-4.local>
2019-04-11 10:39:01 -0300

    update framewrok textfield

---
+++ FLMaterialTextField/FLMaterialTextField.h
@@ -0,0 +1,18 @@
+//
+//  FLMaterialTextField.h
+//  FLMaterialTextField
+//
+//  Created by K2 - Felipe Augusto Pimentel P on 11/04/19.
+//  Copyright © 2019 K2 - Felipe Augusto Pimentel P. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for FLMaterialTextField.
+FOUNDATION_EXPORT double FLMaterialTextFieldVersionNumber;
+
+//! Project version string for FLMaterialTextField.
+FOUNDATION_EXPORT const unsigned char FLMaterialTextFieldVersionString[];
+
+#import <MaterialTextField/MFTextField.h>
+#import <MaterialTextField/UIColor+MaterialTextField.h>
